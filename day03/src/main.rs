6a008885a8e56646cbded5d2db77ce13e2a130ad
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2025-02-03 15:26:30 +0000

    Day 3 - optimise by reusing intersection calculation

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
 use std::fs;
 
-fn path_coords(wire_path: &[&str]) -> HashMap<(i32, i32), u32> {
+type CoordMap = HashMap<(i32, i32), u32>;
+
+fn path_coords(wire_path: &[&str]) -> CoordMap {
     let (mut x, mut y) = (0, 0);
     let mut coords = HashMap::new();
     let mut steps = 0;
@@ -24,33 +26,27 @@ fn path_coords(wire_path: &[&str]) -> HashMap<(i32, i32), u32> {
     coords
 }
 
-fn shortest_distance(wire_path1: &[&str], wire_path2: &[&str]) -> u32 {
-    let coords1 = path_coords(wire_path1);
-    let coords2 = path_coords(wire_path2);
-    let mut intersections = Vec::new();
-    for &coord in coords1.keys() {
-        if coords2.contains_key(&coord) {
-            intersections.push(coord);
-        }
-    }
-    // closest intersection
-    let (x, y) = intersections
-        .iter()
-        .min_by(|(x1, y1), (x2, y2)| (x1.abs() + y1.abs()).cmp(&(x2.abs() + y2.abs())))
-        .expect("wires should intersect at least once");
-
-    x.unsigned_abs() + y.unsigned_abs()
-}
-
-fn fewest_steps(wire_path1: &[&str], wire_path2: &[&str]) -> u32 {
-    let coords1 = path_coords(wire_path1);
-    let coords2 = path_coords(wire_path2);
+fn find_intersections(coords1: CoordMap, coords2: &CoordMap) -> CoordMap {
     let mut intersections = HashMap::new();
     for (coord, steps1) in coords1 {
         if let Some(steps2) = coords2.get(&coord) {
             intersections.insert(coord, steps1 + steps2);
         }
     }
+    intersections
+}
+
+fn shortest_distance(intersections: &CoordMap) -> u32 {
+    // closest intersection
+    let (x, y) = intersections
+        .keys()
+        .min_by(|(x1, y1), (x2, y2)| (x1.abs() + y1.abs()).cmp(&(x2.abs() + y2.abs())))
+        .expect("wires should intersect at least once");
+
+    x.unsigned_abs() + y.unsigned_abs()
+}
+
+fn fewest_steps(intersections: &CoordMap) -> u32 {
     *intersections
         .values()
         .min()
@@ -63,11 +59,14 @@ fn main() {
         .lines()
         .map(|line| line.trim().split(',').collect())
         .collect();
+    let coords1 = path_coords(&wires[0]);
+    let coords2 = path_coords(&wires[1]);
+    let intersections = find_intersections(coords1, &coords2);
 
-    let part1 = shortest_distance(&wires[0], &wires[1]);
+    let part1 = shortest_distance(&intersections);
     println!("{part1}");
 
-    let part2 = fewest_steps(&wires[0], &wires[1]);
+    let part2 = fewest_steps(&intersections);
     println!("{part2}");
 }
 
@@ -117,37 +116,51 @@ mod tests {
         assert_eq!(actual, expected);
     }
 
+    fn assert_shortest_distance(wire1: &str, wire2: &str, expected: u32) {
+        let path1: Vec<_> = wire1.split(',').collect();
+        let path2: Vec<_> = wire2.split(',').collect();
+        let coords1 = path_coords(&path1);
+        let coords2 = path_coords(&path2);
+        let intersections = find_intersections(coords1, &coords2);
+        assert_eq!(shortest_distance(&intersections), expected);
+    }
+
     #[test]
     fn part1_examples() {
-        let path1: Vec<_> = "R8,U5,L5,D3".split(',').collect();
-        let path2: Vec<_> = "U7,R6,D4,L4".split(',').collect();
-        assert_eq!(shortest_distance(&path1, &path2), 6);
+        assert_shortest_distance("R8,U5,L5,D3", "U7,R6,D4,L4", 6);
+        assert_shortest_distance(
+            "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+            "U62,R66,U55,R34,D71,R55,D58,R83",
+            159,
+        );
+        assert_shortest_distance(
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+            135,
+        );
+    }
 
-        let path1: Vec<_> = "R75,D30,R83,U83,L12,D49,R71,U7,L72".split(',').collect();
-        let path2: Vec<_> = "U62,R66,U55,R34,D71,R55,D58,R83".split(',').collect();
-        assert_eq!(shortest_distance(&path1, &path2), 159);
-
-        let path1: Vec<_> = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
-            .split(',')
-            .collect();
-        let path2: Vec<_> = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".split(',').collect();
-        assert_eq!(shortest_distance(&path1, &path2), 135);
+    fn assert_fewest_steps(wire1: &str, wire2: &str, expected: u32) {
+        let path1: Vec<_> = wire1.split(',').collect();
+        let path2: Vec<_> = wire2.split(',').collect();
+        let coords1 = path_coords(&path1);
+        let coords2 = path_coords(&path2);
+        let intersections = find_intersections(coords1, &coords2);
+        assert_eq!(fewest_steps(&intersections), expected);
     }
 
     #[test]
     fn part2_examples() {
-        let path1: Vec<_> = "R8,U5,L5,D3".split(',').collect();
-        let path2: Vec<_> = "U7,R6,D4,L4".split(',').collect();
-        assert_eq!(fewest_steps(&path1, &path2), 30);
-
-        let path1: Vec<_> = "R75,D30,R83,U83,L12,D49,R71,U7,L72".split(',').collect();
-        let path2: Vec<_> = "U62,R66,U55,R34,D71,R55,D58,R83".split(',').collect();
-        assert_eq!(fewest_steps(&path1, &path2), 610);
-
-        let path1: Vec<_> = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
-            .split(',')
-            .collect();
-        let path2: Vec<_> = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".split(',').collect();
-        assert_eq!(fewest_steps(&path1, &path2), 410);
+        assert_fewest_steps("R8,U5,L5,D3", "U7,R6,D4,L4", 30);
+        assert_fewest_steps(
+            "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+            "U62,R66,U55,R34,D71,R55,D58,R83",
+            610,
+        );
+        assert_fewest_steps(
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+            410,
+        );
     }
 }
